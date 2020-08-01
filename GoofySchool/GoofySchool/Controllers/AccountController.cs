23a373f8b54ff5a33c4e05da8bf77d0e4637aeae
laga5508 <52760556+laga5508@users.noreply.github.com>
2020-07-31 18:42:59 -0700

    Broken LogON
    Broken LogON Null exeption Problem

---
+++ GoofySchool/GoofySchool/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using GoofySchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace GoofySchool.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        { }
+
+        private IUserRepository userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public ActionResult LogOut()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            return Redirect("~/");
+        }
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userRepository.LogIn(model.UserName, model.Password);
+                if (user != null)
+                {
+                    System.Web.Security.FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    return Redirect(returnUrl);
+                }
+
+                ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ GoofySchool/GoofySchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoofySchool.MvcApplication" Language="C#" %>
